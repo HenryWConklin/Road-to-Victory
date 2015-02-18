28de3fae1fd488547ff0a35903ebb4c14bd2d66e
Henry Conklin <henrywconklin@gmail.com>
2015-02-18 12:20:51 -0700

    Added update method to main loop

--- RoadToVictory/core/src/edu/virginia/sgd/Game.java
+++ RoadToVictory/core/src/edu/virginia/sgd/Game.java
@@ -5,23 +5,35 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.utils.TimeUtils;
 
 public class Game extends ApplicationAdapter {
 	SpriteBatch batch;
 	Texture img;
+	long lastTime;
 	
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
 		img = new Texture("badlogic.jpg");
+		lastTime = TimeUtils.millis();
 	}
 
 	@Override
 	public void render () {
+		
+		long currentTime = TimeUtils.millis();
+		update(currentTime - lastTime);
+		lastTime = currentTime;
+		
 		Gdx.gl.glClearColor(1, 0, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		batch.begin();
 		batch.draw(img, 0, 0);
 		batch.end();
+	}
+	
+	private void update(long timePassed) {
+		
 	}
 }
